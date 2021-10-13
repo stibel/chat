0ab4910b19f97888370f896fb0e337c370a9161a
stibel <mikolaj.siebielec@skysnap.pl>
2021-10-13 21:12:29 +0200

    Changed tsconfig in both folders, added dist folder, removed built files

--- client/src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var socket_io_client_1 = require("socket.io-client");
-console.log('Client running');
-var socket = (0, socket_io_client_1.io)('http://localhost:3000/');
-socket.emit('chat message', 'Hello');
