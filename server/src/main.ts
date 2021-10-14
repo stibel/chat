f0eaec745731b2e6dee3292edc9065694db744e3
kalin8900 <kalin8900@gmail.com>
2021-10-14 17:45:25 +0200

    basic sockets

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,14 +7,23 @@ const app = express();
 
 const server = app.listen(port, (): void => console.log(`listening to ${port}`));
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3001'
+    }
+});
 
 io.on('connection', socket => {
 
+    console.log(`New client connected ${socket.id}`)
+
     socket.join(ROOMS.ALL)
 
-    socket.on(MESSAGES.ALL, (msg: any) => {
+    socket.on(MESSAGES.ALL, (msg: any, callback: any) => {
         socket.broadcast.to(ROOMS.ALL).emit(MESSAGES.ALL, msg)
+        callback({
+            msg: 'received'
+        })
     })
 
     
