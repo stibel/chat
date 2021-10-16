b9802a10af13ed295122749df4103086b98e8c34
stibel <mikolaj.siebielec@skysnap.pl>
2021-10-16 15:05:50 +0200

    Made messages display in a nicer way

--- client/client/components/Chat.tsx
+++ client/client/components/Chat.tsx
@@ -1,7 +1,13 @@
-import { useEffect, useRef, useState } from "react"
+import { SyntheticEvent, useEffect, useRef, useState } from "react"
 import { useSocket } from "../contexts/SocketContext"
 
 interface ChatProps { }
+
+interface Message {
+    message: string,
+    time: string,
+    sender: boolean | null
+}
 
 export const Chat = (props: ChatProps) => {
 
@@ -9,38 +15,65 @@ export const Chat = (props: ChatProps) => {
 
     const { socket, setSocket } = useSocket()
 
-    const [messages, setMessages] = useState<Array<{ message: string, time: Date, id: number }>>([])
-
-    let id = 0;
+    const [messages, setMessages] = useState<Array<Message>>([])
 
     useEffect(() => {
         socket?.on('all', (msg) => {
             const { message, time } = msg
-            setMessages(prevState => [...prevState, { message: message, time: time.toLocaleString(), id: ++id }])
+            setMessages(prevState => [...prevState, { message: message, time: time.toLocaleString(), sender: null }])
         })
     }, [socket])
 
     useEffect(() => console.log(messages), [messages]);
-
     const onSend = () => {
 
         const value = loginRef.current?.value
-
-        socket?.emit('all', { message: value, time: new Date() }, (msg: any) => console.log(msg))
+        if (value) {
+            const message = { message: value, time: new Date().toLocaleString(), sender: true } //in the future check if sender name is the same as username
+            setMessages(prevState => [...prevState, message])
+            socket?.emit('all', message, (msg: any) => console.log(msg))
+            loginRef.current.value = '';
+        }
     }
 
     return (
         <div>
-            <div style={{ height: '90vh', width: '100vw', overflowY: 'scroll' }}>
-                {messages?.map(message => <div key={message.id}>{message.message} {message.time}</div>)}
+            <div style={{
+                padding: 0,
+                margin: 0,
+                top: 0,
+                height: '90vh',
+                width: '100vw',
+                display: "flex",
+                flexFlow: "column",
+                alignItems: 'center',
+                justifyContent: "center",
+                overflowY: 'scroll',
+                backgroundColor: 'pink'
+            }}>
+                {messages?.map(message =>
+                    <div
+                        key={message.time.toString()}
+                        style={{
+                            minWidth: '20vw',
+                            minHeight: '5vh',
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: 'center',
+                            borderRadius: '5px',
+                            marginTop: '5px',
+                            backgroundColor: message.sender ? 'white' : 'lightblue'
+                        }}
+                    >
+                        {message.message} {message.time}
+                    </div>)}
             </div>
-            <input ref={loginRef} />
-            <button>
-                Login
-            </button>
-            <button onClick={onSend}>
-                Send
-            </button>
+            <div style={{ height: '10vh', width: '100vw', display: "flex", flexFlow: "row" }}>
+                <input style={{ width: '90vw' }} ref={loginRef} />
+                <button style={{ width: '10vw' }} onClick={onSend}>
+                    Send
+                </button>
+            </div>
         </div>
     )
 }
